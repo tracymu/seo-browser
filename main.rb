939e9e5b3ff2553c717e5b88f913745a5378ee44
tracymu <tracy.musung@moomumedia.com>
2014-03-26 16:59:54 +1100

    Added meta description, formatted Google result

--- main.rb
+++ main.rb
@@ -1,28 +1,8 @@
-# If we wanted to take a URL and then just return in a browser an adjusted version of the html
-# Do we need a database?
-# I think maybe I don't need a database
-
-# Need to remember to make sure it takes http:// if not, add it on.
-# loops through all the elements - e.g all the h2s, or whatever.
-# how to do it, with just
-# read more about nokogiri
-# get html, then find/replacing.
-# Press enteri nstead of submit
-
-
-
 require 'Nokogiri'
 require 'open-uri'
 require 'sinatra'
 require 'data_mapper'
-
-
-
-# url = "http://www.nokogiri.org"
-# doc = Nokogiri::HTML(open(url))
-
-# @title = doc.at_css("h2").text
-
+require 'uri/http'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/main.db")
 
@@ -67,15 +47,54 @@ def UrlScrape
   @h5_array = doc.css('h5')
   @h6_array = doc.css('h6')
   @title = doc.at_css("title").text
+  meta_desc = doc.css("meta[name='description']").first
+
+  if meta_desc
+    @content = meta_desc['content']
+  else
+    @content = "This page has no description, Google will choose what content to show from your page, and it will be up to approx 155 characters long"
+  end
+
   @body = doc.css("body")
   @links = doc.css("a")
   @total_links = @links.length
   @external_links = @links.reject{|x| x["href"][0] == '/'}
   @external_links = @external_links.reject{|x| x["href"].include? @url.content}
   @external_links = @external_links.reject{|x| x["href"][0] == '#'}
-  # # Is there a better way to do this than on multiple lines?
+    # # Is there a better way to do this than on multiple lines?
   # # Why is it working for some URLs (beat smh abc) and not others (vision moomux)
-  # @total_external_links = @external_links.length
+  @total_external_links = @external_links.length
+
+end
+
+
+# I want to use this method below to make it so that people can enter wierd URLs in and it will figure it out O
+# Or should I just use validations?
+# I was also thinking knowing the domain would help exclude internal links
+
+# def DomainName(url)
+#   uri = URI.parse(url)
+#   domain = PublicSuffix.parse(uri.host).domain
+# end
+
+
+
+
+###### NOTES AND QUESTIONS ####################
+
+
+# Use this to see about improving the way you get the links http://stackoverflow.com/questions/856706/extract-links-urls-with-nokogiri-in-ruby-from-a-href-html-tags
+
+# then if you do a while loop to print them, you can use an if statement to skip over the ones you dont' want?
+
+# Do we need a database? I think maybe I don't need a database
+
+# Need to remember to make sure it takes http:// if not, add it on.
+
+# How to be able to press enteri nstead of  have to click submit
+
+
+# Obviously the file should probably not be set out like this.
 
 # Make an array of all the things you don't want to include, like #, javascript, @url.content
 
@@ -94,13 +113,13 @@ def UrlScrape
 
 # canonical tag checker-
 
+# How to get meta description out
+# Why aren't the params in the URL, and so I can just refresh?
+
 # need to make it so that .includ? @url.content is just the domain, not www. or http:, so that
 # it can include subdomains
 
-
-
-end
-
-
-# Ask someone how they would actually set all this out.
+# Links to exclude are like this:
+ # links_that_start_with = ['/','#',]
+  # links_that_include = [@domain, 'javascript', 'void']
 
