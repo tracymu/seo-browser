cb5c6c62db287cfc363dcd5dba2cb91cdb7a8db6
tracymu <tracy.musung@moomumedia.com>
2014-04-06 10:46:17 +1000

    Lists external links

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
+#NEXT THING TO DO IS TO MAKE THE DOMAIN A THING SO THAT WE CAN
+# EXCLUDE INTERNAL LINKS - LOOK AT THE NEW GEM I INSTALLED AND THE METHOD I MADE
+
+# ALSO NEED TO FIGURE OUT WHY GETTING THE EXTERNAL LINKS NOT WORKING FOR MOOMU VISION DIRECT. ETC.
+
+
 require 'Nokogiri'
 require 'open-uri'
 require 'sinatra'
@@ -22,11 +28,11 @@ get '/' do
 end
 
 post '/' do
-  u = URL.new
-  u.content = params[:content]
-  u.created_at = Time.now
-  u.updated_at = Time.now
-  u.save
+  url = URL.new
+  url.content = params[:content]
+  url.created_at = Time.now
+  url.updated_at = Time.now
+  url.save
   redirect '/results'
 end
 
@@ -40,12 +46,14 @@ end
 def UrlScrape
 
   doc = Nokogiri::HTML(open(@url.content))
+
   @h1_array = doc.css('h1')
   @h2_array = doc.css('h2')
   @h3_array = doc.css('h3')
   @h4_array = doc.css('h4')
   @h5_array = doc.css('h5')
   @h6_array = doc.css('h6')
+
   @title = doc.at_css("title").text
   meta_desc = doc.css("meta[name='description']").first
 
@@ -57,15 +65,34 @@ def UrlScrape
 
   @body = doc.css("body")
   @links = doc.css("a")
+  @link_list = @links.map { |link| link['href']}
   @total_links = @links.length
-  @external_links = @links.reject{|x| x["href"][0] == '/'}
-  @external_links = @external_links.reject{|x| x["href"].include? @url.content}
-  @external_links = @external_links.reject{|x| x["href"][0] == '#'}
-    # # Is there a better way to do this than on multiple lines?
-  # # Why is it working for some URLs (beat smh abc) and not others (vision moomux)
-  @total_external_links = @external_links.length
 
+  @domain = @url.content.sub(/^https?\:\/\//, '').sub(/^www./,'')
+
+  @external_links = []
+  @internal_links =[]
+
+  @link_list = @link_list.compact  #Some of the links were coming up not as strings, but as nil Class, so had to remove
+
+  @link_list.each do |link|
+    if link.include? @domain
+      @internal_links << link
+    elsif link.start_with? "#"
+      @internal_links << link
+    elsif link.start_with? "/"
+      @internal_links << link
+    elsif link.include? "javascript"
+      @internal_links << link
+    else
+      @external_links << link
+    end
+  end
+    @external_links.reject! { |link| link.empty? }
+    @total_external_links = @external_links.length
 end
+
+
 
 
 # I want to use this method below to make it so that people can enter wierd URLs in and it will figure it out O
@@ -80,7 +107,10 @@ end
 
 
 
+
 ###### NOTES AND QUESTIONS ####################
+
+# Design Patterns for Sinatra apps- how to lay this out better - sinatra chassis
 
 
 # Use this to see about improving the way you get the links http://stackoverflow.com/questions/856706/extract-links-urls-with-nokogiri-in-ruby-from-a-href-html-tags
@@ -122,4 +152,6 @@ end
 # Links to exclude are like this:
  # links_that_start_with = ['/','#',]
   # links_that_include = [@domain, 'javascript', 'void']
+
+  # What happens if there is nothing at that page?
 
